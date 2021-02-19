f90b23f77c07414b2033ebac952f1881442f6359
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-02-19 22:56:15 +0200

    Create add_user.sh

---
+++ add_user.sh
@@ -0,0 +1,21 @@
+printf ${CYAN}"Enter the username for your NON ROOT user\n>"
+read username
+username="${username,,}"
+
+useradd -m -g users -G audio,video,network,wheel,storage,rfkill -s /bin/bash $username
+
+while true; do
+    printf ${LIGHTGREEN}"enter the password for your user %s\n>" $username
+    read -s password
+    printf ${LIGHTGREEN}"re-enter the password for %s\n>" "$username"
+    read -s password_compare
+    if [ "$password" = "$password_compare" ]; then
+	echo "$username:$password" | chpasswd
+        break
+    else
+        printf ${LIGHTRED}"passwords do not match, re enter them\n"
+        printf ${WHITE}".\n"
+        sleep 3
+        clear
+    fi
+done
