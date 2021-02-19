01a061b27c39dac8584569a8a3918bc9e231df0b
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-02-19 22:58:30 +0200

    Update after_reboot.sh

--- after_reboot.sh
+++ after_reboot.sh
@@ -22,27 +22,7 @@ printf ${WHITE}"### Installing bash-completion\n"
 pacman -S bash-completion
 
 printf ${WHITE}"### Setting up non-root user\n"
-printf ${CYAN}"Enter the username for your NON ROOT user\n>"
-read username
-username="${username,,}"
-
-useradd -m -g users -G audio,video,network,wheel,storage,rfkill -s /bin/bash $username
-
-while true; do
-    printf ${LIGHTGREEN}"enter the password for your user %s\n>" $username
-    read -s password
-    printf ${LIGHTGREEN}"re-enter the password for %s\n>" "$username"
-    read -s password_compare
-    if [ "$password" = "$password_compare" ]; then
-	echo "$username:$password" | chpasswd
-        break
-    else
-        printf ${LIGHTRED}"passwords do not match, re enter them\n"
-        printf ${WHITE}".\n"
-        sleep 3
-        clear
-    fi
-done
+./add_user.sh
 
 printf ${WHITE}
 
